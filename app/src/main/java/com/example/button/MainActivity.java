f0dc8893357ebd04c5b7f3a5218426e23d758f9e
Alexandr <penkov-sasha@mail.ru>
2024-02-16 11:05:56 +0300

    пример

---
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+        imageButton = findViewById(R.id.imageButton);
+    }
+
+    public void onClickButtonStart(View view) {
+        imageButton.setImageResource(R.drawable.android);
+        textView.setVisibility(View.VISIBLE);
+        imageButton.setClickable(false);
+    }
+}
